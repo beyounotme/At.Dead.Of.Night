80f4d5dd5688112f9ce8c933b7511d481128abcd
beyounotme <158006622+beyounotme@users.noreply.github.com>
2024-01-30 19:20:52 -0500

    Update greenworks.js
    Signed-off-by: beyounotme <158006622+beyounotme@users.noreply.github.com>

--- At Dead Of Night data/At Dead Of Night/package.nw/greenworks.js
+++ At Dead Of Night data/At Dead Of Night/package.nw/greenworks.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-// The source code can be found in https://github.com/greenheartgames/greenworks
+// The source code can be found in https://github.com/greenheartgames/greenworks/
 var fs = require('fs');
 
 var greenworks;
